9112ec5a38df239d30c8f02df42bab19bc640eb9
AaronEnglish <aaenglis@gmail.com>
2019-06-19 02:24:11 -0500

    added bot template
    added C# bot template

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus;
+using System.Text.RegularExpressions;
+using DSharpPlus.Entities;
+namespace MCStreamBot
+{
+    public class Program
+    {
+        static DiscordClient discord;
+        static DiscordChannel discordChannel;
+        static void Main(string[] args)
+        {
+            switch (args.Length == 0)
+            {
+                case false:
+                    for (int i = 0; i < args.Length; i++)
+                    {
+                        args[i] = Regex.Escape(args[i]);
+                    }
+                    break;
+
+                default:
+                    Console.Write("please supply arguments to issue to discord server"+Environment.NewLine+"Press any key to exit:");
+                    Console.ReadKey();
+                    Environment.Exit(Environment.ExitCode);
+                    break;
+            }
+            try
+            {
+                MainDiscord(args).ConfigureAwait(false).GetAwaiter().GetResult();
+                Environment.Exit(Environment.ExitCode);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        static async Task MainDiscord(string[] args)
+        {
+            discord = new DiscordClient(new DiscordConfiguration
+            {
+                Token = "Discord token here",
+                TokenType = TokenType.Bot
+            });
+            //Discord channel id gets plugged into here
+            discordChannel = discord.GetChannelAsync(11111111111).Result;
+
+            await discord.ConnectAsync();
+            DiscordMessage discordMessage = await discord.SendMessageAsync(discordChannel,args[0],false,null);
+            await discord.DisconnectAsync();
+        }
+    }
+}
