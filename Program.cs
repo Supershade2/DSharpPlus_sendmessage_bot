4f71aa85dceb80347ba6e77f5b51877d62038cf3
AaronEnglish <aaenglis@gmail.com>
2020-11-02 11:51:54 -0600

    Updated Template
    Template updated will now require three arguments, first argument is the token, second is the channel, and finally the third is the message

--- Program.cs
+++ Program.cs
@@ -6,26 +6,44 @@ using System.Threading.Tasks;
 using DSharpPlus;
 using System.Text.RegularExpressions;
 using DSharpPlus.Entities;
+using System.Globalization;
+using Microsoft.Security.Application;
+using Newtonsoft.Json;
 namespace MCStreamBot
 {
     public class Program
     {
+        static MessageData message;
         static DiscordClient discord;
         static DiscordChannel discordChannel;
         static void Main(string[] args)
         {
-            switch (args.Length == 0)
+            switch (args.Length < 3)
             {
                 case false:
-                    for (int i = 0; i < args.Length; i++)
+                    args[2] = Sanitizer.GetSafeHtmlFragment(args[2]);
+                    /*for (int i = 0; i < args.Length; i++)
                     {
                         args[i] = Regex.Escape(args[i]);
-                    }
+                        char[] arg_string = args[i].ToCharArray();
+                        string temp = "";
+                        for (int i1 = 0; i1 < arg_string.Length; i1++)
+                        {
+                            if (arg_string[i1] == 92)
+                            {
+
+                            }
+                            else
+                            {
+                                temp += arg_string[i1];
+                            }
+                        }
+                        args[i] = temp;
+                    }*/
                     break;
 
                 default:
-                    Console.Write("please supply arguments to issue to discord server"+Environment.NewLine+"Press any key to exit:");
-                    Console.ReadKey();
+                    Console.Write("please supply a token, channel number, and message to issue to discord server");
                     Environment.Exit(Environment.ExitCode);
                     break;
             }
@@ -36,23 +54,58 @@ namespace MCStreamBot
             }
             catch (Exception)
             {
-
                 throw;
             }
         }
         static async Task MainDiscord(string[] args)
         {
+            string json;
+            TimeSpan span = TimeSpan.Zero;
+            DiscordMessage discordMessage;
             discord = new DiscordClient(new DiscordConfiguration
             {
-                Token = "Discord token here",
+                Token = args[0],
                 TokenType = TokenType.Bot
             });
             //Discord channel id gets plugged into here
-            discordChannel = discord.GetChannelAsync(11111111111).Result;
-
+            discordChannel = discord.GetChannelAsync(Convert.ToUInt64(args[1])).Result;
             await discord.ConnectAsync();
-            DiscordMessage discordMessage = await discord.SendMessageAsync(discordChannel,args[0],false,null);
+            if (System.IO.File.Exists(args[2].Split(' ')[0] + "lastlive.json")) 
+            {
+                json = System.IO.File.ReadAllText(args[2].Split(' ')[0] + "lastlive.json");
+                message = JsonConvert.DeserializeObject<MessageData>(json);
+                span = DateTimeOffset.Now - message.timeOffset;
+            }
+            switch (span == TimeSpan.Zero)
+            {
+                case true:
+                    discordMessage = await discord.SendMessageAsync(discordChannel, args[2], false, null);
+                    message.timeOffset = discordMessage.CreationTimestamp;
+                    message.message = args[2];
+                    json = JsonConvert.SerializeObject(message);
+                    System.IO.File.WriteAllText(args[2].Split(' ')[0] + "lastlive.json", json);
+                    break;
+                default:
+                    if(span.TotalSeconds < 30)
+                    { 
+                        
+                    }
+                    else 
+                    {
+                        discordMessage = await discord.SendMessageAsync(discordChannel, args[2], false, null);
+                        message.timeOffset = discordMessage.CreationTimestamp;
+                        message.message = args[2];
+                        json = JsonConvert.SerializeObject(message);
+                        System.IO.File.WriteAllText(args[2].Split(' ')[0] + "lastlive.json", json);
+                    }
+                    break;
+            }
             await discord.DisconnectAsync();
+        }
+        public struct MessageData 
+        {
+            public DateTimeOffset timeOffset { get; set; }
+            public string message { get; set; }
         }
     }
 }
